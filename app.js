14df7e378d36d5fcdcb058d4e1121355ef377b45
owuracoder <richagyen20@yahoo.com>
2021-12-07 07:47:50 +0100

    centering div

---
+++ app.js
@@ -0,0 +1,10 @@
+// async function currentWeather(){
+//     const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=london&units=metric&appid=10db400606fb6ced08dc3d4a1196becf',{mode:'cors'})
+
+//     const weatherData = await response.json()
+
+//     console.log(weatherData.main)
+
+// }
+
+// currentWeather()
